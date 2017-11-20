de16488ed353701102b2db9629ecf9144f9a31e8
miguel133331 <miguel.mendoza@mapasydatos.com>
2017-11-20 12:08:28 -0500

    Diseño del menu y de banner principal

---
+++ js/custom.js
@@ -0,0 +1,156 @@
+jQuery(function ($) { "use strict";
+	
+	/* ========================================================================= */
+	/*	Page Preloader
+	/* ========================================================================= */
+	
+	window.onload = function () {
+		document.getElementById('loading-mask').style.display = 'none';
+	}
+
+		
+	/* ========================================================================= */
+	/*	Nice Scroll - Custom Scrollbar
+	/* ========================================================================= */
+
+	var nice = $("html").niceScroll({
+		cursorborderradius: 0,
+		cursorwidth: "8px",
+		cursorfixedheight: 150,
+		cursorcolor: "#515151",
+		zindex: 9999,
+		cursorborder: 0,
+	});
+
+
+	/* ========================================================================= */
+	/*	Scroll Up / Back to top
+	/* ========================================================================= */
+	
+	$(window).scroll(function() {
+		if ($(window).scrollTop() > 400) {
+			$("#scrollUp").fadeIn(200);
+		} else {
+			$("#scrollUp").fadeOut(200);
+		}
+	});
+	
+	$('#scrollUp').click(function() {
+		$('html, body').stop().animate({
+			scrollTop : 0
+		}, 1500, 'easeInOutExpo');
+	});
+
+
+	/* ========================================================================= */
+	/*	Menu item highlighting
+	/* ========================================================================= */
+
+
+	$("#header").sticky({
+		topSpacing : 0
+	});
+	
+	$('#nav').onePageNav({
+		currentClass: 'current',
+		changeHash: false,
+		scrollSpeed: 1500,
+		scrollThreshold: 0.5,
+		filter: '',
+		easing: 'easeInOutExpo'
+	});
+	
+
+
+		
+
+	/* ========================================================================= */
+	/*	Timer count
+	/* ========================================================================= */
+
+	(function() {
+		var count = {
+		  initialized : false,
+		  initialize : function() {
+			if (this.initialized)
+			  return;
+			this.initialized = true;
+			this.build();
+		  },
+		  build : function() {
+			this.animations();
+		  },
+		  animations : function() {
+			// Count To
+			$(".counters-item [data-to]").each(function() {
+			  var $this = $(this);
+			  $this.appear(function() {
+				$this.countTo({});
+			  }, {
+				accX : 0,
+				accY : -150
+			  });
+			});
+		  },
+		};
+		count.initialize();
+	})();
+
+
+	/* ========================================================================= */
+	/*	Skills Chart
+	/* ========================================================================= */
+
+	$(".chart").appear(function () {
+		$(".chart").easyPieChart({
+			easing: "easeOutBounce",
+			barColor: "#515151",
+			size: "150",
+			lineWidth: 10,
+			animate: 2e3,
+			scaleColor: false,
+			onStep: function (e, t, n) {
+				$(this.el).find(".percent").text(Math.round(n))
+			}
+		})
+	});
+	
+
+	/* ========================================================================= */
+	/*	Portfolio Filtering Hook
+	/* =========================================================================  */
+	 var containerEl1 = document.querySelector('[data-ref="container-1"]');
+	 var containerEl2 = document.querySelector('[data-ref="container-2"]');
+
+	 var config = {
+		 controls: {
+			 scope: 'local'
+		 }
+	 };
+
+	 var mixer1 = mixitup(containerEl1, config);
+	 var mixer1 = mixitup(containerEl2, config);				 
+					 
+					 
+});
+
+/* ========================================================================= */
+/*	On scroll fade/bounce fffect
+/* ========================================================================= */
+
+	wow = new WOW({
+		animateClass: 'animated',
+		offset: 120
+	});
+	wow.init();
+/* ========================================================================= */
+/*	Parallax Sections
+/* ========================================================================= */
+
+
+"use strict";
+
+$(window).bind("load", function () {
+   //parallaxInit()
+});
+                            
