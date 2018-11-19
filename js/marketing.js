3d273563ceafac1f23e4e390ec2a71148a4e5189
miigangls <miigangls@gmail.com>
2018-11-18 21:05:01 -0500

    file js for marketing

---
+++ js/marketing.js
@@ -0,0 +1,84 @@
+jQuery(function ($) { "use strict";
+
+/* ========================================================================= */
+/*	Home Slideshow Vegas
+/* ========================================================================= */
+
+$(function() {
+   $('body').vegas({
+       slides: [
+           { src: './img/body/img-body-1.jpg' },
+           { src: './img/body/img-body-2.jpg' },
+           { src: './img/body/img-body-3.jpeg' },
+           { src: './img/body/img-body-4.jpg' },
+           { src: './img/body/img-body-5.jpg' }
+       ],
+       timer: false,
+       transition: [ 'zoomIn', ],
+       animation: ['kenburns']
+   });
+
+ });
+
+/* ========================================================================= */
+/*	Nice Scroll - Custom Scrollbar
+/* ========================================================================= */
+
+var nice = $("html").niceScroll({
+  cursorborderradius: 0,
+  cursorwidth: "8px",
+  cursorfixedheight: 150,
+  cursorcolor: "#035",
+  zindex: 9999,
+  cursorborder: 0,
+});
+
+/* ========================================================================= */
+/*	Menu item highlighting
+/* ========================================================================= */
+
+$('#nav').onePageNav({
+  currentClass: 'current',
+  changeHash: false,
+  scrollSpeed: 1500,
+  scrollThreshold: 0.5,
+  filter: '',
+  easing: 'easeInOutExpo'
+});
+
+/* ========================================================================= */
+/*	Portfolio Filtering Hook
+/* =========================================================================  */
+
+ var containerEl1 = document.querySelector('[data-ref="container-1"]');
+
+ var config = {
+   controls: {
+     scope: 'local'
+   }
+ };
+
+ var mixer1 = mixitup(containerEl1, config);
+
+});
+
+/* ========================================================================= */
+/*	On scroll fade/bounce fffect
+/* ========================================================================= */
+
+wow = new WOW({
+  animateClass: 'animated',
+  offset: 120
+});
+wow.init();
+
+/* ========================================================================= */
+/*	Parallax Sections
+/* ========================================================================= */
+
+
+"use strict";
+
+$(window).bind("load", function () {
+ //parallaxInit()
+});
