ff355ba2ad35bfb07e270bb8f8b1f9e88f6b8de5
miigangls <miigangls@gmail.com>
2019-03-28 01:09:21 -0500

    add css and js for sequence

--- js/main.js
+++ js/main.js
@@ -34,6 +34,8 @@
          animation: ['kenburns']
      });
 
+
+
    });
 
 
@@ -293,6 +295,21 @@
 
 	 var mixer1 = mixitup(containerEl1, config);
 	 var mixer1 = mixitup(containerEl2, config);
+
+
+	 		   // Get the Sequence element
+  var sequenceElement = document.getElementById("sequence");
+
+  var options = {
+    // Place your Sequence options here to override defaults
+    // See: https://sequencejs.com/documentation/#options
+		  
+			keyNavigation: true,
+    fadeStepWhenSkipped: false
+  }
+
+  // Launch Sequence on the element, and with the options we specified above
+  var mySequence = sequence(sequenceElement, options);
 
 
 });
