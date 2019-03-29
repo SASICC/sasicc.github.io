f5ad5ddafddc0f2c68b50798f6d3dac8d558b707
miigangls <miigangls@gmail.com>
2019-03-28 21:56:08 -0500

    delete js sequence for slider

--- js/main.js
+++ js/main.js
@@ -33,23 +33,7 @@
          transition: [ 'zoomIn', ],
          animation: ['kenburns']
      });
-
-		  		   // Get the Sequence element
-  var sequenceElement = document.getElementById("sequence");
-
-  var options = {
-    // Place your Sequence options here to override defaults
-    // See: https://sequencejs.com/documentation/#options
-		autoPlay: true,
-		autoPlayInterval: 500,
-		keyNavigation: true,
-    fadeStepWhenSkipped: false
-  }
-
-  // Launch Sequence on the element, and with the options we specified above
-  var mySequence = sequence(sequenceElement, options);
-
-   });
+});
 
 
 /*
