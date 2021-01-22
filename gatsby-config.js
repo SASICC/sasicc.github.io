f867cca6a781a7cb4e121772fc84806431b694c9
miigangls <miigangls@gmail.com>
2021-01-22 17:06:53 -0500

    create project gatsby react.js

---
+++ gatsby-config.js
@@ -0,0 +1,32 @@
+const path = require(`path`);
+module.exports = {
+    plugins: [
+        `gatsby-plugin-react-helmet`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+              name: `images`,
+              path: path.join(__dirname, `src`, `images`),
+            },
+        },
+        `gatsby-plugin-sharp`,
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-less`,
+        `gatsby-plugin-sharp`,
+        {
+            resolve: `gatsby-source-strapi`,
+            options: {
+                apiURL: process.env.API_URL || `http://localhost:1337`,
+                queryLimit: 5000,
+                contentTypes: ['cargos',
+                    'clientes', 
+                    'habilidades', 
+                    'portafolio', 
+                    'menu', 
+                    'textos', 
+                    'nosotros'
+                ]
+            }
+        }
+    ],
+}
