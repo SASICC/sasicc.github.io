7162cc233ab82bf06f8987b3086d123b3a06cf72
miigangls <miigangls@gmail.com>
2021-01-24 19:22:36 -0500

    menu dinamic.

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -10,14 +10,14 @@ const NavBar = () => {
 			</div>
 			<nav id="nav">
 				<ul>
-					<li ><a href="#banner">Inicio</a></li>
+					<li key="banner" ><a href="#banner">Inicio</a></li>
 					{
 						USEMENU.map(({value, nombre})=>{
 							return <li key={`#${value}`}><a href={`#${value}`}>{nombre}</a></li>
 						})		
 					}
 					<li key='contact'><a href="#footer">Contacto</a></li>
-					<li key='contact'><a href="">Ingresar</a></li>
+					<li key='sing'><a href="">Ingresar</a></li>
 				</ul>
 			</nav>
 		</header>
