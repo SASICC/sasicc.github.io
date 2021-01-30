171892ef23fc7e06bbe6bff1bb8a4f708673e4fa
miigangls <miigangls@gmail.com>
2021-01-30 02:50:26 -0500

    sort folder systems.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import * as React from "react"
 import {Layout,Banner,SEO,About, NavBar, WrapperServices, WrapperTeam,WrapperPortafolio } from "../components";
-import bodyImg  from '../images/img-body-1.jpg'
-import {  } from "../styles/style.less";
+import bodyImg  from '../assets/images/img-body-1.jpg'
+import "../assets/styles/style.less";
 
 export default function IndexPage( ){
   return (
