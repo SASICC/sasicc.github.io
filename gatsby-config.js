f4bbe279eb5e25fbec758204bea954cd0a842945
miigangls <miigangls@gmail.com>
2021-01-30 22:17:42 -0500

    config gatsby  source filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
             resolve: `gatsby-source-filesystem`,
             options: {
               name: `images`,
-              path: path.join(__dirname, `src`, `images`),
+              path: path.join(__dirname, `src`, `assets/images`),
             },
         },
         `gatsby-plugin-sharp`,
