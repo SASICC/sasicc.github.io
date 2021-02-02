87e4f26ed515e8d6cc08133f1ef123253cd42f62
miigangls <miigangls@gmail.com>
2021-02-01 20:03:15 -0500

    erro pages 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,52 +1,21 @@
 import * as React from "react"
 import { Link } from "gatsby"
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
 
 // markup
 const NotFoundPage = () => {
   return (
-    <main style={pageStyles}>
-      <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
+    <main className="error-page" id="wrapper" >
+          <div className="error-box">
+              <div className="error-body text-center">
+                  <h1>404</h1>
+                  <h3 className="text-uppercase">ESTE SITIO ESTA EN MANTENIMIENTO NO ESTARA DISPONIBLE.. </h3>
+                  <p className="text-muted m-t-30 m-b-30">Por favor, intenta después de un tiempo</p>
+                  <footer className="footer text-center">© 2018 SASICC SAS.</footer>
+              </div>
+              <Link style={{margin:'20px 0 0 0'}} className='button button-blue' to="/">Regresar</Link>.
+          </div>
+        
     </main>
   )
 }
